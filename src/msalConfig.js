d6dd99bd9d82df4e506ed816973093ef1e25b488
Rajeesh Durai Thanga Raja <rajeeshdurai.thangaraja@kanini.com>
2023-12-23 22:23:50 +0530

    redirect url

--- src/msalConfig.js
+++ src/msalConfig.js
@@ -3,7 +3,7 @@ export const msalConfig = {
     auth: {
       clientId: '79e59671-0915-4523-a6f8-34035f4c5aa1',
       authority: 'https://your-b2c-tenant.b2clogin.com/your-b2c-tenant.onmicrosoft.com/B2C_1_SuSi',
-      redirectUri: 'https://b2-c-logout-sample-8psr.vercel.app/', // Your redirect URI
+      redirectUri: 'https://AssessmentDemo.b2clogin.com/AssessmentDemo.onmicrosoft.com/B2C_1_SuSi/oauth2/v2.0/logout', // Your redirect URI
     },
     cache: {
       cacheLocation: 'sessionStorage',
