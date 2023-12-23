1753d29d7fdaf734d369741e7ad5af5646c17d03
Rajeesh Durai Thanga Raja <rajeeshdurai.thangaraja@kanini.com>
2023-12-23 21:54:16 +0530

    config file added

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { MsalProvider } from '@azure/msal-react';
 import { msalConfig } from './msalConfig'; // Your MSAL configuration
 import LogoutButton from './component/LogoutButton';
+import { PublicClientApplication } from '@azure/msal-browser';
 
 const App = () => {
+  const msalInstance = new PublicClientApplication(msalConfig);
   return (
-    <MsalProvider instance={msalConfig}>
+    <MsalProvider instance={msalInstance}>
       <div>
         <LogoutButton />
       </div>
