b5f0020ab92dbaa32d81b55b1f8511b2052c8342
Rajeesh Durai <127297740+Rajeesh-Durai@users.noreply.github.com>
2023-12-24 13:00:58 +0530

    Update msalConfig.js

--- src/msalConfig.js
+++ src/msalConfig.js
@@ -4,6 +4,7 @@ export const msalConfig = {
       clientId: '79e59671-0915-4523-a6f8-34035f4c5aa1',
       authority: 'https://AssessmentDemo.b2clogin.com/AssessmentDemo.onmicrosoft.com/B2C_1_SuSi',
       redirectUri: 'https://localhost:3000/', // Your redirect URI
+      postLogoutRedirectUri: 'https://localhost:3000/'
     },
     cache: {
       cacheLocation: 'sessionStorage',
