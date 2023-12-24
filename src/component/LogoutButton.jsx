793dd0b96e649bab063a66599379d3d7b194b52a
Rajeesh Durai Thanga Raja <rajeeshdurai.thangaraja@kanini.com>
2023-12-24 18:35:38 +0530

    Updtaed redirect url

--- src/component/LogoutButton.jsx
+++ src/component/LogoutButton.jsx
@@ -1,20 +1,26 @@
 import React from 'react';
 import { useMsal } from '@azure/msal-react';
+import { PublicClientApplication } from '@azure/msal-browser';
+import { msalConfig } from './msalConfig'; 
 
 const LogoutButton = () => {
   const { instance, accounts } = useMsal();
+  const msalInstance = new PublicClientApplication(msalConfig);
 
   const handleLogout = () => {
-    console.log(accounts)
-    console.log(instance)
-    const logoutRequest = {
-      account: accounts[0], // Assuming there is only one account
-      postLogoutRedirectUri: process.env.REACT_APP_POST_LOGOUT_REDIRECT_URI, // Set this in your environment variables
-    };
+    // console.log(accounts)
+    // console.log(instance)
+    // const logoutRequest = {
+    //   account: accounts[0], // Assuming there is only one account
+    //   postLogoutRedirectUri: process.env.REACT_APP_POST_LOGOUT_REDIRECT_URI, // Set this in your environment variables
+    // };
 
-    instance.logoutPopup(logoutRequest).catch((error) => {
-      console.error('Logout failed:', error);
-    });
+    // instance.logoutPopup(logoutRequest).catch((error) => {
+    //   console.error('Logout failed:', error);
+    // });
+
+    msalInstance.logout();      // Optionally, redirect to the Azure AD B2C logout endpoint  
+    window.location.href = "https://AssessmentDemo.b2clogin.com/AssessmentDemo.onmicrosoft.com/B2C_1_SuSi/oauth2/v2.0/logout";
   };
 
 
--- src/msalConfig.js
+++ src/msalConfig.js
@@ -1,13 +1,13 @@
 // msalConfig.js
 export const msalConfig = {
-    auth: {
-      clientId: '79e59671-0915-4523-a6f8-34035f4c5aa1',
-      authority: 'https://AssessmentDemo.b2clogin.com/AssessmentDemo.onmicrosoft.com/B2C_1_SuSi',
-      redirectUri: 'https://localhost:3000/', // Your redirect URI
-    },
-    cache: {
-      cacheLocation: 'sessionStorage',
-      storeAuthStateInCookie: false,
-    },
-  };
-  
+  auth: {
+    clientId: '79e59671-0915-4523-a6f8-34035f4c5aa1',
+    authority: 'https://AssessmentDemo.b2clogin.com/AssessmentDemo.onmicrosoft.com/B2C_1_SuSi',
+    redirectUri: 'https://localhost:3000/', // Your redirect URI
+    postLogoutRedirectUri: 'https://AssessmentDemo.b2clogin.com/AssessmentDemo.onmicrosoft.com/B2C_1_SuSi/oauth2/v2.0/logout'
+  },
+  cache: {
+    cacheLocation: 'sessionStorage',
+    storeAuthStateInCookie: false,
+  },
+};
