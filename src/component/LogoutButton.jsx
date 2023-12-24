28da04c1f40baa0f8f2dbb8795242f6fae125184
Rajeesh Durai Thanga Raja <rajeeshdurai.thangaraja@kanini.com>
2023-12-24 19:27:47 +0530

    Updated LogOutButton and msalConfig

--- src/component/LogoutButton.jsx
+++ src/component/LogoutButton.jsx
@@ -1,33 +1,26 @@
 import React from 'react';
 import { useMsal } from '@azure/msal-react';
 import { PublicClientApplication } from '@azure/msal-browser';
-import { msalConfig } from '../msalConfig'; 
+import { msalConfig } from '../msalConfig';
 
 const LogoutButton = () => {
-  const { instance, accounts } = useMsal();
+  const { instance } = useMsal();
   const msalInstance = new PublicClientApplication(msalConfig);
 
-  const handleLogout = () => {
-    // console.log(accounts)
-    // console.log(instance)
-    // const logoutRequest = {
-    //   account: accounts[0], // Assuming there is only one account
-    //   postLogoutRedirectUri: process.env.REACT_APP_POST_LOGOUT_REDIRECT_URI, // Set this in your environment variables
-    // };
-
-    // instance.logoutPopup(logoutRequest).catch((error) => {
-    //   console.error('Logout failed:', error);
-    // });
-
-    msalInstance.logout();      // Optionally, redirect to the Azure AD B2C logout endpoint  
-    window.location.href = "https://AssessmentDemo.b2clogin.com/AssessmentDemo.onmicrosoft.com/B2C_1_SuSi/oauth2/v2.0/logout";
+  const handleLogout = async () => {
+    try {
+      await instance.logoutPopup(); // Use logoutPopup to show a popup during logout
+    } catch (error) {
+      console.error('Logout failed:', error);
+    } finally {
+      // Redirect to your post-logout page after the session is cleared
+      window.location.href = "https://AssessmentDemo.b2clogin.com/AssessmentDemo.onmicrosoft.com/B2C_1_SuSi/oauth2/v2.0/logout?p=B2C_1_SuSi";
+    }
   };
-
-
 
   return (
     <div>
-        <button onClick={handleLogout}>Logout</button>
+      <button onClick={handleLogout}>Logout</button>
     </div>
   );
 };
