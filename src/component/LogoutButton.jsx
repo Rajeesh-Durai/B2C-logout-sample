e2dfb746e82b343c228f9aa10a4df7ab99cfbdcb
Rajeesh Durai <127297740+Rajeesh-Durai@users.noreply.github.com>
2023-12-24 18:22:46 +0530

    Update LogoutButton.jsx

--- src/component/LogoutButton.jsx
+++ src/component/LogoutButton.jsx
@@ -5,16 +5,20 @@ const LogoutButton = () => {
   const { instance, accounts } = useMsal();
 
   const handleLogout = () => {
-    console.log(accounts)
-    console.log(instance)
-    const logoutRequest = {
-      account: accounts[0], // Assuming there is only one account
-      postLogoutRedirectUri: process.env.REACT_APP_POST_LOGOUT_REDIRECT_URI, // Set this in your environment variables
-    };
+    // console.log(accounts)
+    // console.log(instance)
+    // const logoutRequest = {
+    //   account: accounts[0], // Assuming there is only one account
+    //   postLogoutRedirectUri: process.env.REACT_APP_POST_LOGOUT_REDIRECT_URI, // Set this in your environment variables
+    // };
 
-    instance.logoutPopup(logoutRequest).catch((error) => {
-      console.error('Logout failed:', error);
-    });
+    // instance.logoutPopup(logoutRequest).catch((error) => {
+    //   console.error('Logout failed:', error);
+    // });
+
+    [6:10 PM] Rajnesh Durai Thanga Raja
+  msalInstance.logout();      // Optionally, redirect to the Azure AD B2C logout endpoint  
+    window.location.href = "https://AssessmentDemo.b2clogin.com/AssessmentDemo.onmicrosoft.com/B2C_1_SuSi/oauth2/v2.0/logout"; };
   };
 
 
